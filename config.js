18aafd045b831af731f0895bcb59ffb76a7ac6f4
msimmainofficial <msimmainofficial@gmail.com>
2026-07-27 16:26:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ const SUPABASE_URL = "https://cmtttmuiwgwmdqgcztgc.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_LEJNJGx5T1BHk9JpnRD8Qg_0j1O_enx";
 
 const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
 );
+
 alert("Config Loaded");
